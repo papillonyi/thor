5f6272c7d4e7bb4f44bebaa83ed21b0c4791081d
Andy <cy39@live.cn>
2019-03-12 16:03:36 +0800

    rm foreignkey

--- model/migrate.go
+++ model/migrate.go
@@ -8,16 +8,7 @@ import (
 var db, _ = gorm.Open("mysql", "root:root@tcp(mysql-1:3306)/thor?charset=utf8mb4&parseTime=True&loc=Local")
 
 func Migrate() {
-	//db, err := gorm.Open("mysql", "root:root@/thor?charset=utf8mb4&parseTime=True&loc=Local")
-	//if err != nil {
-	//	panic("failed to connect database")
-	//}
-	//defer db.Close()
 	db.LogMode(true)
 	db.AutoMigrate(&CurrencyType{}, &CurrencyRate{}, &MyCurrencyData{}, &Task{})
-	db.Model(&CurrencyRate{}).AddForeignKey("source_currency_id", "currency_types(id)", "CASCADE", "RESTRICT")
-	db.Model(&CurrencyRate{}).AddForeignKey("to_currency_id", "currency_types(id)", "CASCADE", "RESTRICT")
-	db.Model(&MyCurrencyData{}).AddForeignKey("currency_id", "currency_types(id)", "CASCADE", "RESTRICT")
 
-	//UpdateAllExchangeRate(db)
 }
